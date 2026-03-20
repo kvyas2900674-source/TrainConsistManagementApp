81cc189edd410fdb54abbdf0f42fc12f36532c78
kvyas2900674-source <kvyas.2900674>
2026-03-20 09:48:47 +0530

    UC3: Track Unique Bogie IDs using HashSet

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,27 +1,27 @@
+import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
 
         System.out.println("=======================================");
-        System.out.println(" UC4 - Maintain Ordered Bogie IDs ");
+        System.out.println(" UC3 - Track Unique Bogie IDs ");
         System.out.println("=======================================\n");
 
-        // Create train consist using TreeSet (SortedSet behavior)
-        Set<String> trainConsist = new TreeSet<>();
+        // Create train consist using HashSet
+        Set<String> trainConsist = new HashSet<>();
 
-        // Adding bogies (out of order intentionally)
-        trainConsist.add("B3");
+        // Adding bogies
         trainConsist.add("B1");
         trainConsist.add("B2");
-        trainConsist.add("B2"); // Duplicate (ignored)
+        trainConsist.add("B3");
+        trainConsist.add("B2"); // Duplicate
 
-        // Display sorted train consist
-        System.out.println("Bogies added (automatically sorted & duplicates removed).\n");
+        // Display train consist
+        System.out.println("Bogies added (duplicates automatically ignored).\n");
 
-        System.out.println("Current Train Consist (Sorted):");
+        System.out.println("Current Train Consist:");
         for (String bogie : trainConsist) {
             System.out.println(bogie);
         }
