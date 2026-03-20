db7d90d8e49ec9129bbf9679356db35368b67757
kvyas2900674-source <kvyas.2900674>
2026-03-20 09:39:57 +0530

    UC1: Initialize Train and Display Consist Summary

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println(" UC1 - Initialize Train and Display Consist Summary ");
+        System.out.println("=======================================\n");
+
+        // Create empty train consist
+        List<String> trainConsist = new ArrayList<>();
+
+        // Display initial state
+        System.out.println("Train initialized successfully.");
+        System.out.println("Initial Bogie Count: " + trainConsist.size());
+    }
 }
