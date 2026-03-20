2c122c28f8dc29072d9ab74ee786e94436475a74
kvyas2900674-source <kvyas.2900674>
2026-03-20 09:49:38 +0530

    UC5: Preserve Insertion Order of Bogies using LinkedHashSet

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,4 +1,4 @@
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class TrainConsistManagementApp {
@@ -6,22 +6,22 @@ public class TrainConsistManagementApp {
     public static void main(String[] args) {
 
         System.out.println("=======================================");
-        System.out.println(" UC3 - Track Unique Bogie IDs ");
+        System.out.println(" UC5 - Preserve Insertion Order of Bogies ");
         System.out.println("=======================================\n");
 
-        // Create train consist using HashSet
-        Set<String> trainConsist = new HashSet<>();
+        // Create train consist using LinkedHashSet
+        Set<String> trainConsist = new LinkedHashSet<>();
 
-        // Adding bogies
+        // Adding bogies (in a specific order)
+        trainConsist.add("B3");
         trainConsist.add("B1");
         trainConsist.add("B2");
-        trainConsist.add("B3");
-        trainConsist.add("B2"); // Duplicate
+        trainConsist.add("B2"); // Duplicate (ignored)
 
         // Display train consist
-        System.out.println("Bogies added (duplicates automatically ignored).\n");
+        System.out.println("Bogies added (insertion order preserved, duplicates removed).\n");
 
-        System.out.println("Current Train Consist:");
+        System.out.println("Current Train Consist (Insertion Order):");
         for (String bogie : trainConsist) {
             System.out.println(bogie);
         }
